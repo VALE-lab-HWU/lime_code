2d7f7e3ff158a6d7903c86292559c17ca8fa2178
jackred <jackred@tuta.io>
2021-06-18 12:32:42 +0100

    ADD dendrogram help function in model helper

--- model_helper.py
+++ model_helper.py
@@ -1,6 +1,7 @@
 from sklearn.cluster import KMeans
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.cluster import AgglomerativeClustering
 
 
 # return a kmeans instance from sklearn
@@ -32,3 +33,14 @@ def run_model(x_train, y_train, test, **kwargs):
     model = get_model(x_train, y_train, **kwargs)
     print('Test model')
     return model.predict(test)
+
+
+# do dendrogram
+def build_dendrogram_model():
+    return AgglomerativeClustering(compute_distances=True)
+
+
+def fit_dendrogram(data):
+    model = build_dendrogram_model()
+    model.fit(data)
+    return model
